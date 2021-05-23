b518783fd7a122e2404d437f354f4dba85f460c1
master-ladvil <52162632+master-ladvil@users.noreply.github.com>
2021-05-23 15:58:42 +0530

    Add files via upload

--- routes/initflagentry.js
+++ routes/initflagentry.js
@@ -5,7 +5,7 @@ module.exports = function( req,res,next){
         competitor : req.body.studentName
     })
     const findexist =  flagentrymod.findOne({"competitor" : req.body.studentName})
-    if(findexist){res.send("you have already registered")}
+    if(findexist){return res.send("you have already registered")}
     try{
         const initialised = user.save()
 
