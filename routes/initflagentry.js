55a0e2c8895adf3bcf7f67248f4fefc1b7d89996
master-ladvil <52162632+master-ladvil@users.noreply.github.com>
2021-05-26 13:00:58 +0530

    Delete routes directory

--- routes/initflagentry.js
+++
@@ -1,7 +0,0 @@
-const flagentrymod = require("../models/flagentrymodel")
-
-module.exports = function( req,res,next){
-    
-    const findexist =  flagentrymod.findOne({competitor : req.body.studentName})
-    console.log(findexist)
-}
